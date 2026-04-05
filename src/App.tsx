5c761b618f41d26f2ee6aadfa2a3ee53319d0236
ashley-perez <ashleyperez428@gmail.com>
2026-04-05 14:49:37 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+
+function ATestButton({ title }: { title: string }) {
+  return (
+    <button>{title}</button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my placeholder</h1>
+      <ATestButton title="I'm a button" />
+    </div>
+  );
+}
+
